a28ab238cbdde33e23b0701ce011ff9fb6208d7e
johnrenko <vishnou@gmail.com>
2016-02-07 18:00:25 +1100

    fixed the active state

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,30 +4,46 @@
  * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { propTypes, Component } from 'react';
 import './Navigation.scss';
 import Link from '../Link';
-import Location from "../../core/Location";
 
 class Navigation extends Component {
+  constructor() {
+    super();
+    this.state = {loading: true};
+  }
+
+  componentDidMount() {
+  var self = this;
+  setTimeout(() => {
+    self.setState({loading: false}); }, 1);
+  }
 
   render() {
-    return (
-      <ul className="Navigation" role="menu">
-        <li className="Navigation-item">
-          <a className={`${location.pathname === "/" ? "active" : ""} Navigation-link`} href="/" onClick={Link.handleClick}>Me</a>
-        </li>
-        <li className="Navigation-item">
-          <a className={`${location.pathname === "/process" ? "active" : ""} Navigation-link`} href="/process" onClick={Link.handleClick}>Process</a>
-        </li>
-        <li className="Navigation-item">
-          <a className={`${location.pathname === "/casestudies" ? "active" : ""} Navigation-link`} href="/casestudies" onClick={Link.handleClick}>Case studies</a>
-        </li>
-        <li className="Navigation-item">
-          <a className={`${location.pathname === "/contact" ? "active" : ""} Navigation-link`} href="/contact" onClick={Link.handleClick}>Contact</a>
-        </li>
-      </ul>
-    );
+    if (this.state.loading) {
+        return(
+          <div />
+        )
+      }
+    else {
+      return(
+          <ul className="Navigation" role="menu">
+            <li className="Navigation-item">
+              <a className={`${location.pathname === "/" ? "active" : ""} Navigation-link`} href="/" onClick={Link.handleClick}>Me</a>
+            </li>
+            <li className="Navigation-item">
+              <a className={`${location.pathname === "/process" ? "active" : ""} Navigation-link`} href="/process" onClick={Link.handleClick}>Process</a>
+            </li>
+            <li className="Navigation-item">
+              <a className={`${location.pathname === "/casestudies" ? "active" : ""} Navigation-link`} href="/casestudies" onClick={Link.handleClick}>Case studies</a>
+            </li>
+            <li className="Navigation-item">
+              <a className={`${location.pathname === "/contact" ? "active" : ""} Navigation-link`} href="/contact" onClick={Link.handleClick}>Contact</a>
+            </li>
+          </ul>
+        )
+      }
   }
 }
 
