7714ea6ad40abfa75da27d5900acb82ffa6435a8
johnrenko <vishnou@gmail.com>
2016-02-23 21:04:45 +1100

    did first page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,14 @@ export default class extends Component {
 		<section>
   	    	<div className="content">   
           		<h1>Me</h1>
-          		<p>Coming soon.</p>
+              <div className="box">
+                <h2 className="meTitle3">John Dutamby, UX Designer</h2>
+                <p>A UX Designer with 5+ years of experience going from research to product design. </p>
+                <p>Looking for simple solutions to complex problems, transforming constraints into opportunities. </p>
+                <p>I strongly believe that with the right <strong>motivation, communication and organisation</strong>, any team can achieve the best possible outcome. </p>
+              </div>
+              <div className="boxImg">
+              </div>
         	</div>
     </section>
   </div>
