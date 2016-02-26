169c0466759ac886f4f31ffca2b6032712ac5527
johnrenko <vishnou@gmail.com>
2016-02-26 23:12:24 +1100

    experimenting svg waves

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,21 @@ export default class extends Component {
     return (
 	<div>
 		<section>
-  	    	<div className="content">   
-          		<h1>Me</h1>
-              <div className="box">
+  	    	<div className="color1">   
+            <div className="content">
+                  <h1>Me</h1>
+            </div>
+          </div>
+          <svg viewBox="0 0 1280 110" preserveAspectRatio="none" id="header-curve" data-reactid=".20qcf4exa80.1.0.$=1$/.0.1"><path d="M12800 -26.1V110H0C194 71.33 642-19.9 1280 3.9z" fill="#29BBE1" data-reactid=".20qcf4exa80.1.0.$=1$/.0.1.0"></path></svg>
+          <div className="content">
+            <div className="box">
                 <h2 className="meTitle3">John Dutamby, UX Designer</h2>
                 <p>A UX Designer with 5+ years of experience going from research to product design. </p>
                 <p>Looking for simple solutions to complex problems, transforming constraints into opportunities. </p>
                 <p>I strongly believe that with the right <strong>motivation, communication and organisation</strong>, any team can achieve the best possible outcome. </p>
-              </div>
-              <div className="boxImg">
-              </div>
+            </div>
+            <div className="boxImg">
+            </div>
         	</div>
     </section>
   </div>
