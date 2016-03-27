17cecc56351f160517ff05430cad06d631fbfff2
johnrenko <vishnou@gmail.com>
2016-03-27 13:32:30 +1100

    added discovery text

--- pages/process.js
+++ pages/process.js
@@ -20,6 +20,37 @@ export default class extends Component {
           <svg viewBox="0 0 1280 100" preserveAspectRatio="none" id="header-curve-process" data-reactid=".20qcf4exa80.1.0.$=1$/.0.1">
             <path d="M0,100 C0,0 0,0 0,0 S0,0 1280,0" data-reactid=".20qcf4exa80.1.0.$=1$/.0.1.0"></path>
           </svg>
+          <div className="content">
+            <div className="box">
+                <h2 className="processTitle3">Discovery</h2>
+                <h4>What problem are we trying to solve?</h4>
+                <p>On paper, this part seems obvious. In action, it's the easiest to skip.</p>
+                <p>It is so easy to move to solution mode as soon as possible. But that's not how to create great experiences. </p>
+                <p>To find the right solution, you must first understand the problem. And not just at a high level, you must master all the different use cases, data related, possible edge cases.</p>
+                <p>The discovery phase is aimed at creating this knowledge database. This will then help shape the long term vision and will make the sharing with the team more efficient.</p>
+                <p>This part relies on both quantitative and qualitative data. The quantitative data (competitors, market analysis, analytics) will help understand the rationale problem. Qualitative data will then give human answers.</p>
+                <p>Outputs of this phase goes from market analytics to user journeys, personas, audio snippets. The end goal is to create a common understanding of a problem, as well as documents to help share this.
+                It should shape a vision of the future that is shared by the whole team and business.</p>
+            </div>
+            <div className="box">
+                <h2 className="processTitle3">Design</h2>
+                <p>A UX Designer with 5+ years of experience going from research to product design. </p>
+                <p>Looking for simple solutions to complex problems, transforming constraints into opportunities. </p>
+                <p>I strongly believe that with the right <strong>motivation, communication and organisation</strong>, any team can achieve the best possible outcome. </p>
+            </div>
+            <div className="box">
+                <h2 className="processTitle3">Iterations</h2>
+                <p>A UX Designer with 5+ years of experience going from research to product design. </p>
+                <p>Looking for simple solutions to complex problems, transforming constraints into opportunities. </p>
+                <p>I strongly believe that with the right <strong>motivation, communication and organisation</strong>, any team can achieve the best possible outcome. </p>
+            </div>
+            <div className="box">
+                <h2 className="processTitle3">Validation</h2>
+                <p>A UX Designer with 5+ years of experience going from research to product design. </p>
+                <p>Looking for simple solutions to complex problems, transforming constraints into opportunities. </p>
+                <p>I strongly believe that with the right <strong>motivation, communication and organisation</strong>, any team can achieve the best possible outcome. </p>
+            </div>
+          </div>
       	</section>
       	
     </div>
